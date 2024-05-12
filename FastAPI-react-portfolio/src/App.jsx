eb1bb214d40b7a6c288637a135bdf38c2985d92f
Benjamin <bini2nd@yahoo.com>
2024-05-12 12:09:38 +0200

    image.glb issue fixed

--- FastAPI-react-portfolio/src/App.jsx
+++ FastAPI-react-portfolio/src/App.jsx
@@ -1,5 +1,8 @@
 import {Route,BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Home, About, Projects, Contact } from './pages'
 import Navbar from './components/Navbar'
+
+
 
 const App = () => {
   return (
@@ -7,12 +10,12 @@ const App = () => {
         <Router>
             <Navbar/>
             <Routes>
-                <Route path='/' element={"Home"}/>
-                <Route path='/about' element={"About"}/>
-                <Route path='/projects' element={"Projects"}/>
-                <Route path='/contacts' element={"Contacts"}/>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/projects' element={<Projects/>}/>
+                <Route path='/contact' element={<Contact/>}/>
 
-                
+            
             </Routes>
         </Router>
 
--- FastAPI-react-portfolio/src/components/Navbar.jsx
+++ FastAPI-react-portfolio/src/components/Navbar.jsx
@@ -8,6 +8,17 @@ const Navbar = () => {
         <p className='blue-gradient_text'>Mr. B</p>
 
       </NavLink>
+      <nav className='flex text-lg gap-7 font-medium'>
+        <NavLink to="/about" className={({isActive}) => isActive ? "text-blue-500": "text-black"}>
+        About
+        </NavLink>
+        <NavLink to="/projects" className={({isActive}) => isActive ? "text-blue-500": "text-black"}>
+        Projects
+        </NavLink>
+        <NavLink to="/contact" className={({isActive}) => isActive ? "text-blue-500": "text-black"}>
+        Contact
+        </NavLink>
+      </nav>
         
     </header>
   )
