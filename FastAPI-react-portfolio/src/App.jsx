6c95bc19c3612fe2fd5fc50234b3bd4f250f4ec8
Benjamin <bini2nd@yahoo.com>
2024-05-23 20:48:46 +0200

    about page started

--- FastAPI-react-portfolio/src/App.jsx
+++ FastAPI-react-portfolio/src/App.jsx
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <main className='bg-slate-300/100'>
+    <main className='bg-slate-300/20'>
         <Router>
             <Navbar/>
             <Routes>
--- FastAPI-react-portfolio/src/pages/About.jsx
+++ FastAPI-react-portfolio/src/pages/About.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+
+import { skills } from "../constants";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="biniyam-about font-semibold drop-shadow">Biniyam</span>
+      </h1>
 
-export default About
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          I'm am Mechanical engineer with Master's degree in Machine design -
+          but with a twist. But that's not all, I've also dipped my toes into
+          the exciting world of Artificial Intelligence and python development.
+          My journey in AI has led me to develop a deep passion for several
+          cutting-edge technologies. AI, Generative AI, Machine Learning and
+          Data Analysis are all domains I am deeply passionate about. But, I'm
+          not just about the software. My background in Engineering allows me to
+          bring a unique perspective to my work in AI.I am always excited about
+          learning new things and looking for new challanges.
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20">  
+            <div className="btn-back rounded-xl"/>           
+              <div className="btn-front rounded-xl flex justify-center items-center">
+               
+                  <img
+                    src={skill.imageUrl}
+                    alt={skill.name}
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
